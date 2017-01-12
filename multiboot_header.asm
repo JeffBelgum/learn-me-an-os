94691756a3fa45e15019386fdb0a34f4a352eb1a
Jeff Belgum <jeffbelgum@gmail.com>
2017-01-12 00:32:34 -0800

    add multiboot header

---
+++ multiboot_header.asm
@@ -0,0 +1,15 @@
+section .multiboot_header
+header_start:
+  dd 0xe85250d6                ; magic number for multiboot 2
+  dd 0                         ; arch - (protected mode i386)
+  dd header_end - header_start ; header length
+  ; checksum
+  dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+
+  ; multiboot tags here
+
+  ; required end tag
+  dw 0 ; type
+  dw 0 ; flags
+  dd 8 ; size
+header_end:
