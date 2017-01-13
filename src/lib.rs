f4d2435a146f5a3a25a9f144d832d97321c0a3c2
Jeff Belgum <jeffbelgum@gmail.com>
2017-01-12 23:47:59 -0800

    retrieve and print memory areas

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![feature(const_fn, lang_items, unique)]
 #![no_std]
 
+extern crate multiboot2;
 extern crate rlibc;
 extern crate spin;
 extern crate volatile;
@@ -28,17 +29,19 @@ pub extern "C" fn _Unwind_Resume() -> ! {
 
 // main entry point
 #[no_mangle]
-pub extern fn rust_main() {
-
+pub extern fn rust_main(multiboot_information_address: usize) {
     vga_buffer::clear_screen();
-    println!("Hello World{}", "!");
-    println!("{}", { println!("inner"); "outer" });
+
+    let boot_info = unsafe {
+        multiboot2::load(multiboot_information_address)
+    };
+    let memory_map_tag = boot_info.memory_map_tag()
+        .expect("Memory map tag required");
+
+    println!("memory areas:");
+    for area in memory_map_tag.memory_areas() {
+        println!("    start: 0x{:x}, length: 0x{:x}", area.base_addr, area.length);
+    }
 
     loop {}
 }
-
-// fn print_something() {
-//     let cc = ColorCode::new(Color::LightGreen, Color::Black);
-//     let mut writer = Writer::new(cc);
-//     writer.write_byte("Hello World");
-// }
