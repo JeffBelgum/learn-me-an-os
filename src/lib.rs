34fcf476b1939ba30196e7dab46ab32cfc55399e
Jeff Belgum <jeffbelgum@gmail.com>
2017-01-12 23:50:01 -0800

    more informative panic message

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,9 @@ extern fn eh_personality() {
 
 #[lang = "panic_fmt"]
 #[no_mangle]
-pub extern fn panic_fmt() -> ! {
+pub extern fn panic_fmt(fmt: core::fmt::Arguments, file: &'static str, line: u32) -> ! {
+    println!("\n\nPANIC in {} at line {}:", file, line);
+    println!("    {}", fmt);
     loop {}
 }
 
